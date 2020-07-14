91f85f56145b089ed06c079085776ad40b6431e5
Saddam Hussain <engrsaddammahar@gmail.com>
2020-07-15 03:12:54 +0500

    second commit.

--- app/connection/connection.go
+++ app/connection/connection.go
@@ -7,20 +7,26 @@ import (
 )
 
 var Connection *ftp.ServerConn
+
 func Connect() {
 	var err error
-	Connection, err = ftp.Dial("streetram.net:21066", ftp.DialWithTimeout(5*time.Second))
+	addr := "streetram.net:21066"
+	user := "fahad"
+	pass := "@Dfklpt699"
+	Connection, err = ftp.Dial(addr, ftp.DialWithTimeout(5*time.Second))
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error when trying to connect with ftp server.")
+		fmt.Println("Failed to connect with FTP server(" + addr + ").")
 		panic(err)
 	}
-
 	fmt.Println("connection stablished.")
+
 	// login to the ftp server
-	err = Connection.Login("fahad", "@Dfklpt699")
+	err = Connection.Login(user, pass)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error when trying to login on ftp server.")
+		fmt.Println("Failed to login on FTP server(" + addr + ") as (" + user + ").")
 		panic(err)
 	}
-	fmt.Println("LoggedIn as Fahad.")
+	fmt.Println("LoggedIn as: "+user)
 }
